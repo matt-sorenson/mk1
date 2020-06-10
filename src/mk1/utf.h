629369aa0ee75f8012a2443c6c456ce1e98e3d3b
Matt Sorenson <matt@mattsorenson.com>
2020-06-10 16:51:13 -0700

    Use the utf8 namespace instead of providing utility functions custom

--- src/mk1/utf.h
+++ src/mk1/utf.h
@@ -1,41 +1,10 @@
 #pragma once
 
+#include <utf8.h>
+
 namespace mk1 {
 
 typedef std::string string;
-
-bool is_valid_utf8(const char* str, const size_t len);
-bool starts_with_utf8_bom(const char* str, const size_t len);
-
-string to_utf8(const char16_t* str, const size_t len);
-string to_utf8(const char32_t* str, const size_t len);
-
-std::u16string to_utf16(const char* str, const size_t len);
-std::u16string to_utf16(const char32_t* str, const size_t len);
-
-std::u32string to_utf32(const char* str, const size_t len);
-std::u32string to_utf32(const char16_t* str, const size_t len);
-
-inline bool is_valid_utf8(const char* str);
-inline bool starts_with_utf8_bom(const char* str);
-
-inline bool is_valid_utf8(const string& str);
-inline bool starts_with_utf8_bom(const string& str);
-
-template <typename T>
-string to_utf8(const T* str);
-template <typename T>
-std::u16string to_utf16(const T* str);
-template <typename T>
-std::u32string to_utf32(const T* str);
-
-template <typename T>
-string to_utf8(const std::basic_string<T>& str);
-template <typename T>
-std::u16string to_utf16(const std::basic_string<T>& str);
-template <typename T>
-std::u32string to_utf32(const std::basic_string<T>& str);
+using namespace utf8;
 
 } // namespace mk1
-
-#include <mk1/utf.cpp.inl>
